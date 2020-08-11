626ce351e816cb98e8b753362efb5ee1a9b23d09
David Paul Crouch <david.crouch@hck2.com>
2020-08-11 13:50:16 -0500

    testing es6 syntax for headers

--- functions/year/year.js
+++ functions/year/year.js
@@ -1,39 +1,40 @@
-const axios = require("axios");
-const path = require("path");
-const fs = require("fs");
+import axios from "axios";
+import { resolve } from "path";
+import { readFileSync } from "fs";
+
+import { headers } from "../../utils/headers";
 
 const WEEK_IN_SECONDS = 3600 * 24 * 7;
 
-const headers = fs.readFileSync(
-  path.resolve(__dirname) + "/headers.json",
+/* const headers = readFileSync(
+  resolve(__dirname) + "/headers.json",
   "utf-8"
-);
+); */
 
 var options = {
   method: "post",
   url: "https://testws.atdconnect.com/rs/3_6/fitment/year",
   data: {},
-  ...JSON.parse(headers)
+  ...headers
 };
 
-exports.handler = async (event, context, callback) => {
+var years;
+
+export async function handler(event, context, callback) {
+  console.log(headers);
   let cacheHit = true;
 
-  console.log(process.env.YEARS);
-
-  if (!process.env.YEARS) {
-    var req = await axios(options).then(res => (process.env.YEARS = res.data));
+  if (!years) {
+    var req = await axios(options).then(res => (years = res.data));
     cacheHit = false;
   }
-
-  console.log(typeof process.env.YEARS);
 
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       timestamp: Math.floor(Date.now() / 1000),
       cached: cacheHit,
-      data: process.env.YEARS
+      data: years
     })
   });
-};
+}
