31b613ff74d49b8df90f613712c034caa1670084
David Paul Crouch <david.crouch@hck2.com>
2020-08-10 19:14:04 -0500

    updating lambda

--- functions/lambdatest.js
+++ functions/lambdatest.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 var options = {
   method: 'POST',
+  protocol: 'https',
   headers: {
     "Accept": "application/json",
     "Content-Type": "application/json",
@@ -17,7 +18,7 @@ exports.handler = async (event, context, callback) => {
   years = years || await http.request("https://testws.atdconnect.com/rs/3_6/fitment/year",
   options,
   (response) => response)
-  
+
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ years }),
