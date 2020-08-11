e0b990e61ab5c87de89f66c8eeead730278431f3
David Paul Crouch <david.crouch@hck2.com>
2020-08-10 19:07:21 -0500

    updating lambda

--- functions/lambdatest.js
+++ functions/lambdatest.js
@@ -1,15 +1,23 @@
-const path = require("path")
-const fs = require("fs")
+const http = require('http')
 
-var years = fs.readFile(
-  path.resolve(__dirname) + "/store/years.json",
-  "utf8",
-  (err, data) => {
-    console.log(path.resolve(__filename))
+var options = {
+  method: 'POST',
+  headers: {
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "clientId": "HAMMS_TIRE",
+    "Accept-Language": "en-US",
+    "Authorization": "Basic aGFtbXN0aXJlYXBpOmhhbW1zdGlyZVRlc3QyMA=="
   }
-)
+}
 
-exports.handler = (event, context, callback) => {
+var years
+
+exports.handler = async (event, context, callback) => {
+  years = years || await http.request("https://testws.atdconnect.com/rs/3_6/fitment/year",
+  options,
+  (response) => response)
+  
   callback(null, {
     statusCode: 200,
     body: JSON.stringify({ years }),
