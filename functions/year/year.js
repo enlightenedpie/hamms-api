2a81894fbd60bc5e9b03f6b1f32d01136e0fe556
David Paul Crouch <david.crouch@hck2.com>
2020-08-11 12:02:31 -0500

    testing readfile method

--- functions/year/year.js
+++ functions/year/year.js
@@ -1,22 +1,30 @@
 const axios = require("axios");
+const path = require("path");
+const fs = require("fs");
+
+const WEEK_IN_SECONDS = 3600 * 24 * 7;
+
+const headers = fs.readFile(
+  path.resolve(__dirname) + "/headers.json",
+  "utf-8",
+  (err, data) => {
+    if (err) throw err;
+    return data;
+  }
+);
 
 var options = {
   method: "post",
   url: "https://testws.atdconnect.com/rs/3_6/fitment/year",
   data: {},
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    clientId: "HAMMS_TIRE",
-    "Accept-Language": "en-US",
-    Authorization: "Basic aGFtbXN0aXJlYXBpOmhhbW1zdGlyZVRlc3QyMA=="
-  }
+  ...headers
 };
 
 var years;
 
 exports.handler = async (event, context, callback) => {
   let cacheHit = true;
+
   if (!years) {
     var req = await axios(options).then(res => (years = res.data));
     cacheHit = false;
@@ -24,6 +32,10 @@ exports.handler = async (event, context, callback) => {
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({ cached: cacheHit, data: years })
+    body: JSON.stringify({
+      timestamp: Math.floor(Date.now() / 1000),
+      cached: cacheHit,
+      data: years
+    })
   });
 };
